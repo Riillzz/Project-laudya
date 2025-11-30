d08282c0214c69543bceb33b39321fc8dab5356c
Riillzz <159740645+Riillzz@users.noreply.github.com>
2025-11-30 11:15:38 +0700

    Update responsive-fix.js

--- responsive-fix.js
+++ responsive-fix.js
@@ -1,10 +1,1 @@
-// Hilangkan efek 3D saat layar kecil
-if (window.innerWidth < 768) {
 
-    updateCoverflow = function () {
-        // disable seluruh animasi 3D untuk mode mobile
-        document.querySelectorAll('.coverflow-item').forEach((item) => {
-            item.style.transform = "none";
-        });
-    };
-}
